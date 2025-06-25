c98f12b38a3af876a156100871a3937545241665
HaidaDaniel <danilagjada22@gmail.com>
2025-06-25 18:33:57 +0300

    feat: Remove tanstack router plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig, loadEnv } from 'vite'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -8,7 +7,7 @@ export default defineConfig(({ mode }) => {
   console.log('Proxy target:', env.VITE_API_BASE_URL)
 
   return {
-    plugins: [tanstackRouter({ target: 'react', autoCodeSplitting: true }), react(), tailwindcss()],
+    plugins: [, react(), tailwindcss()],
     server: {
       proxy: {
         '/api': {
